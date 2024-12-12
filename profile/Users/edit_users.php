890677cc065255fe1ef73b2d6d7a44f3375d7041
YlliM1 <ym64822@ubt-uni.net>
2024-12-12 18:30:10 +0100

    Edit Function fix (ignore)

--- profile/Users/edit_users.php
+++ profile/Users/edit_users.php
@@ -2,29 +2,28 @@
 include '../../config.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Fetch the form data
     $id = $_POST['id'];
     $firstName = $_POST['first_name'];
     $lastName = $_POST['last_name'];
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $role = $_POST['role']; // Assuming role is also part of the form data, if not remove this line
+    $role = $_POST['role']; 
 
-    // Update query
+
     $sql = "UPDATE users SET
         first_name='$firstName',
         last_name='$lastName',
         username='$username',
         email='$email',
         password='$password',
-        role='$role'   -- Assuming you want to update the role too
+        role='$role'  
         WHERE id='$id'";
 
-    // Execute the query
+
     if ($conn->query($sql) === TRUE) {
         header('Location: view_users.php');
-        exit; // Ensure the script stops executing after redirection
+        exit; 
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
@@ -40,19 +39,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="stylesheet" href="edit_users.css">
 </head>
 <body>
-    <h2>Edit User</h2>
+
 
     <?php
-    // Assuming the 'id' is passed in the URL or as a parameter
+ 
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
 
-        // Fetch the user's data from the database
         $sql = "SELECT * FROM users WHERE id='$id'";
         $result = $conn->query($sql);
         
         if ($result->num_rows > 0) {
-            // Fetch user data
+       
             $user = $result->fetch_assoc();
         } else {
             echo "User not found.";
@@ -64,33 +62,45 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
     ?>
 
-    <!-- Edit user form -->
-    <form action="edit_users.php" method="POST">
-        <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
-        
+<h2>Edit User:</h2>
+<form action="edit_users.php" method="POST">
+    <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+
+    <div class="row">
         <label for="first_name">First Name:</label>
-        <input type="text" name="first_name" value="<?php echo $user['first_name']; ?>" required><br><br>
-
+        <input type="text" name="first_name" value="<?php echo $user['first_name']; ?>" required>
+    </div>
+    <div class="row">
         <label for="last_name">Last Name:</label>
-        <input type="text" name="last_name" value="<?php echo $user['last_name']; ?>" required><br><br>
+        <input type="text" name="last_name" value="<?php echo $user['last_name']; ?>" required>
+    </div>
 
+    <div class="row">
         <label for="username">Username:</label>
-        <input type="text" name="username" value="<?php echo $user['username']; ?>" required><br><br>
+        <input type="text" name="username" value="<?php echo $user['username']; ?>" required>
+    </div>
 
+    <div class="row">
         <label for="email">Email:</label>
-        <input type="email" name="email" value="<?php echo $user['email']; ?>" required><br><br>
+        <input type="email" name="email" value="<?php echo $user['email']; ?>" required>
+    </div>
 
+    <div class="row">
         <label for="password">Password:</label>
-        <input type="password" name="password" value="<?php echo $user['password']; ?>" required><br><br>
+        <input type="password" name="password" value="<?php echo $user['password']; ?>" required>
+    </div>
 
+    <div class="row">
         <label for="role">Role:</label>
         <select name="role" required>
             <option value="admin" <?php if ($user['role'] == 'admin') echo 'selected'; ?>>Admin</option>
             <option value="user" <?php if ($user['role'] == 'user') echo 'selected'; ?>>User</option>
-            <!-- Add more roles as needed -->
-        </select><br><br>
+        </select>
+    </div>
 
-        <input type="submit" value="Update User">
-    </form>
+    <input type="submit" value="Update User">
+    <a href="../profile/profile.php"><button class="Profile_button">Back to Profile</button></a>
+</form>
+
 </body>
 </html>
