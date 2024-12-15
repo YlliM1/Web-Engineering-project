3a699f17daa2789f05fb89bb54b59cf6e9469f64
flamur9 <fk67828@ubt-uni.net>
2024-12-15 23:51:57 +0100

    Pjesa e About Us

---
+++ AboutUs/aboutUs.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AboutUs</title>
+    <link rel="stylesheet" href="../AboutUs/aboutUs.css">
+</head>
+<body>  
+   
+    <img class="aboutusimg" src="../images/aboutusimg.jpg" alt="Shoes">
+    
+    <h2 class="shoestxt">At Shoes Shop, we are dedicated to providing high-quality footwear that 
+        blends style, comfort, and durability. Our curated collection
+         caters to all tastes, from trendy sneakers to elegant dress 
+         shoes. We believe that shoes are not just accessories, 
+         but an essential part of self-expression. Our mission 
+         is to offer exceptional 
+        customer service and deliver shoes that make every step memorable."
+Feel free to adjust it to reflect your shop's values and personality!</h2> 
+
+
+<div class="container">
+        <div class="text-left">
+            <h2 class="mission">Our Mission</h2>
+            <h2 class="shoestxt2">At Shoes Shop we are committed to providing our customers with a wide selection
+                of high-quality footwear that combines comfort, style, and durability. 
+                Whether you're looking for everyday sneakers, elegant dress shoes, or unique,
+                trendy designs, we have something to fit every lifestyle. Our mission is to help you walk in confidence and comfort,
+                no matter where life takes you.</h2>
+        </div>
+        <div class="image-right">
+            <img class="shoe" src="../images/shoe.png" alt="Shoe">
+        </div>
+    </div>
+
+    
+</body>
+</html>
