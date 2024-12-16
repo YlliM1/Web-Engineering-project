1364d1c83ba196243c1f6a320b3f5af623bb7211
flamur9 <fk67828@ubt-uni.net>
2024-12-16 03:23:25 +0100

    Pjesa e Services

---
+++ Services/services.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="services.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+</head>
+<body>
+    <div class="section">
+        <div class="title">
+           <h1> Our Services </h1>
+        </div>
+
+<div class="services">
+    <div class="card">
+        <div class="icon">
+        <i class="fas fa-shoe-prints"></i>
+        </div>
+            <h2>Personalized Fitting</h2>
+            <p>Offer professional advice on the right shoe size and fit. 
+                This could include measuring the feet, identifying foot arch types, 
+                and recommending shoes that offer the best comfort and support.</p>
+        </div>
+    
+
+    <div class="card">
+        <div class="icon">
+        <i class="fas fa-truck"></i>
+        </div>
+        <h2>Online Shopping</h2>
+            <p>Allow customers to shop online and have shoes delivered directly 
+                to their home. 
+                Provides customers with a hassle-free shopping experience and saves time, especially for 
+                those who can’t visit the physical store.
+                </p>
+        </div>
+    
+
+
+    <div class="card">
+    <div class="icon">
+        <i class="fas fa-money-bill-wave"></i>
+        <div >
+            <h2>Exchange and Return Policy</h2>
+            <p>A flexible exchange or return policy, which can include exchanges
+                 for different sizes or refund options within a certain period.</p>
+        </div>
+    
+
+</div>
+
+</body>
+</html>
+
+    
